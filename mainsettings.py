681b34273dbaabe8736977ef6e087e4d76ea3598
paperbenni <paperbenni@gmail.com>
2020-04-08 20:02:52 +0200

    mouse menu now works

--- mainsettings.py
+++ mainsettings.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
-import webbrowser
+
+#############################################
+## main menu of the instantOS settings app ##
+#############################################
+
 import gi
 gi.require_version('Gtk', '3.0')
-import subprocess
+
+from gi.repository import Gtk
 import pathlib
 import subprocess
-from gi.repository import Gtk
+import webbrowser
 
 # won't show icons otherwise
 settings = Gtk.Settings.get_default()
 settings.props.gtk_button_images = True
+
 
 class Handler:
     def mainwindow_destroy_cb(self, *args):
@@ -22,22 +28,27 @@ class Handler:
     def mousebutton_clicked_cb(self, button):
         mousewin = builder.get_object('mousewindow')
         mousewin.show_all()
-    
+
     def mousecancel_clicked_cb(self, button):
         builder.get_object('mousewindow').destroy()
 
     def networkbutton_clicked_cb(self, button):
         popover = builder.get_object('networkpopover')
         popover.popup()
-    
+
     def appearancebutton_clicked_cb(self, button):
         subprocess.Popen(["lxappearance"])
 
     def displaychange_clicked_cb(self, button):
         subprocess.Popen(['arandr'])
+
+
 builder = Gtk.Builder()
 
-builder.add_from_file("./settings.glade")
+if pathlib.Path('./mainsettings.glade').exists():
+    builder.add_from_file("./mainsettings.glade")
+else:
+    builder.add_from_file("/usr/share/instantsettings/mainsettings.glade")
 
 builder.connect_signals(Handler())
 
--- modules/mouse/mousesettings.py
+++ modules/mouse/mousesettings.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+
+##########################################
+## set mouse sensitivity using libinput ##
+##########################################
+
 from gi.repository import Gtk
 import subprocess
 import pathlib
 import gi
+import os
+
 gi.require_version('Gtk', '3.0')
 
 # won't show icons otherwise
@@ -10,9 +17,14 @@ settings = Gtk.Settings.get_default()
 settings.props.gtk_button_images = True
 
 
-class Handler:
+def applyscale(value):
+    mousefactor = (int(value) - 50) / 50
+    print("setting mouse sensitivity to" + str(mousefactor))
+    subprocess.Popen(["instantmouse", "s", str(mousefactor)])
+    return mousefactor
 
-    mousescale = builder.get_object('mousescale')
+
+class Handler:
 
     def window_destroy_cb(self, *args):
         Gtk.main_quit()
@@ -21,14 +33,19 @@ class Handler:
         builder.get_object('window').destroy()
 
     def okbutton_clicked_cb(self, button):
+        mousescale = builder.get_object('mousescale')
+        newmousefactor = applyscale(mousescale.get_value())
+        subprocess.Popen(["iconf", "mousespeed", str(newmousefactor)])
         builder.get_object('window').destroy()
 
     def applybutton_clicked_cb(self, button):
-        print("frank")
+        mousescale = builder.get_object('mousescale')
+        applyscale(mousescale.get_value())
+
 
 builder = Gtk.Builder()
-
-builder.add_from_file("./mouse.glade")
+builder.add_from_file(os.path.dirname(
+    os.path.realpath(__file__)) + "/mouse.glade")
 
 builder.connect_signals(Handler())
 
