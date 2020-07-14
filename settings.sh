f557f5931a82407927edcdf351f82bb6cafe3c61
paperbenni <paperbenni@gmail.com>
2020-07-14 22:36:39 +0200

    init new settings functionality

--- settings.sh
+++ settings.sh
@@ -14,23 +14,69 @@ asksetting() {
 :b 朗Printing
 :y Wallpaper
 :r Storage
+:y Dotfiles
 >>h Advanced settings
 :b Firewall
 :y TLP
-:r Close Settings' | instantmenu -l 2000 -w 400 -i -h 60 -x 100000 -y 0 -bw 4
+:r Close Settings' | instantmenu -l 2000 -w 400 -i -h 55 -x 100000 -y 0 -bw 4 -H
+}
+
+sidebar() {
+	instantmenu -l 2000 -w 400 -i -h 55 -x 100000 -y 0 -bw 4 -H
+}
+
+displaysettings() {
+	CHOICE="$(echo '>>h Display Settings
+:b Change display settings
+:g Make current settings permanent
+:b Back' | sidebar)"
+
+	case $CHOICE in
+	*Change*)
+		arandr &
+		;;
+	*Make*)
+		autorandr --force --save instantos
+		;;
+	*Back)
+		LOOPSETTING="True"
+		;;
+	esac
+
 }
 
 LOOPSETTING="true"
-while [ -n "$LOOPSETTING" ]
-do
+while [ -n "$LOOPSETTING" ]; do
 	SETTING="$(asksetting)"
 	unset LOOPSETTING
 	case "$SETTING" in
-		*Sound)
-			pavucontrol &
+	*Sound)
+		pavucontrol &
 		;;
-		*Appearance)
-			lxappearance &
+	*Appearance)
+		lxappearance &
+		;;
+	*Software)
+		pamac-manager &
+		;;
+	*Display)
+		displaysettings
+		;;
+	*Keyboard)
+		/opt/instantos/menus/dm/tk.sh 123
+		;;
+	*Printing)
+		system-config-printer &
+		;;
+	*Bluetooth)
+		blueman-assistant &
+		;;
+	*Dotfiles)
+		[ -e ~/.instantrc ] || instantdotfiles
+		st -e "nvim" -c ":e ~/.instantrc" &
+		;;
+	*Power)
+		xfce4-power-manager-settings &
 		;;
 	esac
 done
